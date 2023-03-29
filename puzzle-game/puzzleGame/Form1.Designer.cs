cf1dcda87bea0191a4f241d163b4e3998a1ebf70
Hüseyin Oğuz Çetinkaya <huseyinoguzc@gmail.com>
2023-03-30 02:12:01 +0300

    Versiyon 1

---
+++ puzzle-game/puzzleGame/Form1.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace puzzleGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            original = new System.Windows.Forms.PictureBox();
+            button1 = new System.Windows.Forms.Button();
+            button2 = new System.Windows.Forms.Button();
+            button3 = new System.Windows.Forms.Button();
+            button4 = new System.Windows.Forms.Button();
+            button5 = new System.Windows.Forms.Button();
+            button6 = new System.Windows.Forms.Button();
+            button7 = new System.Windows.Forms.Button();
+            button8 = new System.Windows.Forms.Button();
+            button9 = new System.Windows.Forms.Button();
+            button10 = new System.Windows.Forms.Button();
+            button11 = new System.Windows.Forms.Button();
+            button12 = new System.Windows.Forms.Button();
+            button13 = new System.Windows.Forms.Button();
+            button14 = new System.Windows.Forms.Button();
+            button15 = new System.Windows.Forms.Button();
+            button16 = new System.Windows.Forms.Button();
+            openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            mix = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)original).BeginInit();
+            SuspendLayout();
+            // 
+            // original
+            // 
+            original.BackgroundImage = Properties.Resources.upload;
+            original.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            original.Location = new System.Drawing.Point(12, 12);
+            original.Name = "original";
+            original.Size = new System.Drawing.Size(200, 200);
+            original.TabIndex = 0;
+            original.TabStop = false;
+            original.Click += original_Click;
+            // 
+            // button1
+            // 
+            button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            button1.Location = new System.Drawing.Point(294, 93);
+            button1.Margin = new System.Windows.Forms.Padding(0);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(100, 100);
+            button1.TabIndex = 1;
+            button1.TabStop = false;
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            button2.Location = new System.Drawing.Point(394, 93);
+            button2.Margin = new System.Windows.Forms.Padding(0);
+            button2.Name = "button2";
+            button2.Size = new System.Drawing.Size(100, 100);
+            button2.TabIndex = 2;
+            button2.TabStop = false;
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            button3.Location = new System.Drawing.Point(494, 93);
+            button3.Margin = new System.Windows.Forms.Padding(0);
+            button3.Name = "button3";
+            button3.Size = new System.Drawing.Size(100, 100);
+            button3.TabIndex = 3;
+            button3.TabStop = false;
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            button4.Location = new System.Drawing.Point(594, 93);
+            button4.Margin = new System.Windows.Forms.Padding(0);
+            button4.Name = "button4";
+            button4.Size = new System.Drawing.Size(100, 100);
+            button4.TabIndex = 4;
+            button4.TabStop = false;
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            button5.Location = new System.Drawing.Point(294, 193);
+            button5.Margin = new System.Windows.Forms.Padding(0);
+            button5.Name = "button5";
+            button5.Size = new System.Drawing.Size(100, 100);
+            button5.TabIndex = 5;
+            button5.TabStop = false;
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            button6.Location = new System.Drawing.Point(394, 193);
+            button6.Margin = new System.Windows.Forms.Padding(0);
+            button6.Name = "button6";
+            button6.Size = new System.Drawing.Size(100, 100);
+            button6.TabIndex = 6;
+            button6.TabStop = false;
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            button7.Location = new System.Drawing.Point(494, 193);
+            button7.Margin = new System.Windows.Forms.Padding(0);
+            button7.Name = "button7";
+            button7.Size = new System.Drawing.Size(100, 100);
+            button7.TabIndex = 7;
+            button7.TabStop = false;
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            button8.Location = new System.Drawing.Point(594, 193);
+            button8.Margin = new System.Windows.Forms.Padding(0);
+            button8.Name = "button8";
+            button8.Size = new System.Drawing.Size(100, 100);
+            button8.TabIndex = 8;
+            button8.TabStop = false;
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            button9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            button9.Location = new System.Drawing.Point(294, 293);
+            button9.Margin = new System.Windows.Forms.Padding(0);
+            button9.Name = "button9";
+            button9.Size = new System.Drawing.Size(100, 100);
+            button9.TabIndex = 16;
+            button9.TabStop = false;
+            button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            button10.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            button10.Location = new System.Drawing.Point(394, 293);
+            button10.Margin = new System.Windows.Forms.Padding(0);
+            button10.Name = "button10";
+            button10.Size = new System.Drawing.Size(100, 100);
+            button10.TabIndex = 15;
+            button10.TabStop = false;
+            button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            button11.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            button11.Location = new System.Drawing.Point(494, 293);
+            button11.Margin = new System.Windows.Forms.Padding(0);
+            button11.Name = "button11";
+            button11.Size = new System.Drawing.Size(100, 100);
+            button11.TabIndex = 14;
+            button11.TabStop = false;
+            button11.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            button12.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            button12.Location = new System.Drawing.Point(594, 293);
+            button12.Margin = new System.Windows.Forms.Padding(0);
+            button12.Name = "button12";
+            button12.Size = new System.Drawing.Size(100, 100);
+            button12.TabIndex = 13;
+            button12.TabStop = false;
+            button12.UseVisualStyleBackColor = true;
+            // 
+            // button13
+            // 
+            button13.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            button13.Location = new System.Drawing.Point(294, 393);
+            button13.Margin = new System.Windows.Forms.Padding(0);
+            button13.Name = "button13";
+            button13.Size = new System.Drawing.Size(100, 100);
+            button13.TabIndex = 12;
+            button13.TabStop = false;
+            button13.UseVisualStyleBackColor = true;
+            // 
+            // button14
+            // 
+            button14.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            button14.Location = new System.Drawing.Point(394, 393);
+            button14.Margin = new System.Windows.Forms.Padding(0);
+            button14.Name = "button14";
+            button14.Size = new System.Drawing.Size(100, 100);
+            button14.TabIndex = 11;
+            button14.TabStop = false;
+            button14.UseVisualStyleBackColor = true;
+            // 
+            // button15
+            // 
+            button15.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            button15.Location = new System.Drawing.Point(494, 393);
+            button15.Margin = new System.Windows.Forms.Padding(0);
+            button15.Name = "button15";
+            button15.Size = new System.Drawing.Size(100, 100);
+            button15.TabIndex = 10;
+            button15.TabStop = false;
+            button15.UseVisualStyleBackColor = true;
+            // 
+            // button16
+            // 
+            button16.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            button16.Location = new System.Drawing.Point(594, 393);
+            button16.Margin = new System.Windows.Forms.Padding(0);
+            button16.Name = "button16";
+            button16.Size = new System.Drawing.Size(100, 100);
+            button16.TabIndex = 9;
+            button16.TabStop = false;
+            button16.UseVisualStyleBackColor = true;
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // mix
+            // 
+            mix.Location = new System.Drawing.Point(12, 218);
+            mix.Name = "mix";
+            mix.Size = new System.Drawing.Size(200, 29);
+            mix.TabIndex = 18;
+            mix.Text = "Karıştır";
+            mix.UseVisualStyleBackColor = true;
+            mix.Click += mix_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            ClientSize = new System.Drawing.Size(823, 560);
+            Controls.Add(mix);
+            Controls.Add(button9);
+            Controls.Add(button10);
+            Controls.Add(button11);
+            Controls.Add(button12);
+            Controls.Add(button13);
+            Controls.Add(button14);
+            Controls.Add(button15);
+            Controls.Add(button16);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(original);
+            Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            Name = "Form1";
+            Text = "Puzzle Game";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)original).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox original;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button mix;
+    }
+}
---
+++ puzzle-game/puzzleGame/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace puzzleGame
+{
+    public partial class Form1 : Form
+    {
+        LinkedList<Image> linkedList = new LinkedList<Image>();//Image sınıfı türünden bir bağlı liste oluşturduk
+        LinkedList<Image> mixedLinkedList = new LinkedList<Image>();//Image sınıfı türünden bir bağlı liste daha oluşturduk, bu listeyi karıştırılan parçalar için kullanacağız
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void original_Click(object sender, EventArgs e)
+        {
+            for (int i = 1; i <= 16; i++)//Form ilk açıldığında puzzle parçaları kullanılabilir değildir
+            {
+                var button = Controls.Find($"button{i}", true).FirstOrDefault() as Button;
+                if (button != null)
+                {
+                    button.Enabled = false;
+                }
+            }
+
+            original.SizeMode = PictureBoxSizeMode.StretchImage;//Orijinal resmi PicturBox boyutunda sıkıştırıyoruz
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                mix.Enabled = true;
+                original.Image = Image.FromFile(openFileDialog1.FileName);
+                parcala();
+            }
+        }
+
+        private void parcala()
+        {
+
+            if (original.Image != null)
+            {
+                linkedList.Clear();//Listeyi temizlemezsem her seferinde ilk seçilen görsel gelir çünkü ilk 16 düğüme bü görselin parçaları atandı
+            }
+
+            Bitmap resim = new Bitmap(original.Image, 400, 400);
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Bitmap parca = resim.Clone(new Rectangle(j * 100, i * 100, 100, 100), resim.PixelFormat);
+                    linkedList.AddLast(parca);
+                }
+            }
+
+            LinkedListNode<Image> current = linkedList.First;//Tüm düğümleri sıra sıra gezip içideki parçaları butonlara atamak için Head değerli bir düğüm oluşturdum
+            for (int i = 1; i <= 16; i++)
+            {
+                var button = Controls.Find($"button{i}", true).FirstOrDefault() as Button;
+                if (button != null)
+                {
+                    button.Image = current.Value;
+                    current = current.Next;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 1; i <= 16; i++)//Form ilk açıldığında puzzle parçaları kullanılabilir değildir
+            {
+                var button = Controls.Find($"button{i}", true).FirstOrDefault() as Button;
+                if (button != null)
+                {
+                    button.Enabled = false;
+                }
+            }
+        }
+
+        private void karistir()//Sadece 16. parça doğru yerde olunca en az bir tanesi doğru yerde diye algılamıyor
+        {
+            if (original.Image != null)
+            {
+                mixedLinkedList.Clear();//Listeyi temizlemezsem her seferinde ilk seçilen görsel gelir çünkü ilk 16 düğüme bü görselin parçaları atandı
+            }
+
+            Random rastgele = new Random();
+            LinkedList<Image> copyLinkedList = new LinkedList<Image>(linkedList);
+            for (int i = 0; i < 16; i++)//16 tane parça var 16 kere çalışacak
+            {
+                int rastgeleIndex = rastgele.Next(0, copyLinkedList.Count);//0 ve 16 arasında rastgele bir sayı oluşturuyoruz
+                LinkedListNode<Image> rastgeleNode = copyLinkedList.First;//Image sınıfı türünden bir düğüm elde ediyoruz ve buna düzenli listenin ilk elemanını atıyoruz
+                for (int j = 0; j < rastgeleIndex; j++)
+                {
+                    rastgeleNode = rastgeleNode.Next;//Rastgele sayının üretildiği düğümün bulunduğu yere kadar ilerliyoruz
+                }
+                copyLinkedList.Remove(rastgeleNode);//Karışık listeye alınan düğümü düzgün olan kopya listeden siliyoruz bu sayede aynı parça iki kez işleme alınmıyor
+                mixedLinkedList.AddLast(rastgeleNode);//Karıştırılmış listeye resim parçalı düğüm ekleniyor
+            }
+
+            LinkedListNode<Image> mixedLinkedListNode = mixedLinkedList.First;//Karışık listenin ilk düğümü
+            LinkedListNode<Image> linkedListNode = linkedList.First;//Düzgün listenin ilk düğümü
+            int count = 0;//Her iki listede de parçalar aynıysa artacak olan sayaç
+            while (mixedLinkedListNode.Next != null)
+            {
+                if (mixedLinkedListNode.Value == linkedListNode.Value)
+                {
+                    count++;
+                }
+                mixedLinkedListNode = mixedLinkedListNode.Next;
+                linkedListNode = linkedListNode.Next;
+            }
+
+            if (count >= 1)//En az bir parça doğru yerde mi diye kontrol ediyor
+            {
+                mix.Enabled = false;//Parçalardan en az bir tanesi doğru yerdeyse artık karıştırma butonu aktif olmasın yani karıştırma yapılamasın
+
+                for (int i = 1; i <= 16; i++)//Karıştırma işlemi düzgün şekilde bitti ve karıştırma butonu deaktif oldu puzzle parçaları aktif oldu
+                {
+                    var button = Controls.Find($"button{i}", true).FirstOrDefault() as Button;
+                    if (button != null)
+                    {
+                        button.Enabled = true;
+                    }
+                }
+            }
+
+            //Her bir butona yeni bağlı listedeki sıradaki elemanı atıyoruz
+            LinkedListNode<Image> current = mixedLinkedList.First;//Karışık listenin ilk düğümünü yeni bir düğüme atıyoruz
+            for (int i = 1; i <= 16; i++)
+            {
+                var button = Controls.Find($"button{i}", true).FirstOrDefault() as Button;
+                if (button != null)
+                {
+                    button.Image = current.Value;
+                    current = current.Next;
+                }
+            }
+        }
+
+        private void mix_Click(object sender, EventArgs e)
+        {
+            karistir();
+        }
+
+        //BUTONLARDAKİ RESİMLERİN YERİ DEĞİŞİYOR FAKAT KONUM KONTROLÜNÜ LİNKEDLİST ÜZERİNDEN YAPMAK GEREKİYOR, BUNUN ÜZERİNDE DURULACAK!
+    }
+}
